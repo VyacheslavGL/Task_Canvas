56cfea4e9864585df6da42a551f8fabc9173ffc5
file <iglstivens@yandex.ru>
2020-01-19 14:58:17 +0300

    changed files

---
+++ src/components/Canvas.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-container>
+        <v-layout>
+            <h1>Canvas HTML</h1>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: "Canvas"
+    }
+</script>
+
+<style scoped>
+
+</style>
